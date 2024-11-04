c32aa6b2839e4ae8a7cee0e0e2cf4208ddbf29a3
KIMY0UNGGON <53406529+KIMY0UNGGON@users.noreply.github.com>
2024-11-04 10:35:59 +0900

    서버와 관련된 내용 주석 추가. 대대적으로 추가와 수정 필요

--- client/boardgame/GameMain_Server.cs
+++ client/boardgame/GameMain_Server.cs
@@ -59,7 +59,7 @@ namespace boardgame
 
         }
 
-        public void sendtomainthread(string data)
+        public void sendtomainthread(string data) //서버에서 보내온 값을 받아 쓰레드를 계속해서 실행(멀티쓰레드)
         {
             if (this.InvokeRequired)
             {
@@ -67,15 +67,20 @@ namespace boardgame
                 return;
             }
             //메인쓰레드
-            datadta(data);
+            datadta(data); //서버에서 값을 받아 메인 폼에 적용하거나 서버에 다시 값을 보냄
         }
 
 
 
-        private void datadta(string data)
+        private void datadta(string data) //서버의 데이터를 받아 값에 따라 send와 receive 값을 정해 실행
         {
             //UI 사용 가능
+            //받은 값의 첫번째 값에 따라 실행하는 명령이 달라지게 할 예정.
+            //m 이면 움직임과 관련 (이때 상대방이 움직였으면 바로 다음플레이어에게 턴이 넘어가도록 변경.), s면 건물의 판매, b면 구매.
 
+            //들어온 순서에 따라 서버에서 클라이언트에 번호를 지정해줌. 그 지정한 번호에 따라 자신의 번호를 변경하고 값을 받았음을 서버에 다시 알림.
+            //현재는 색깔이 번호에 따라 지정되어있지만 선착순으로 선택하도록 할 예정. 즉 번호를 지정받고 말을 선택하는 폼을 띄운 뒤 플레이어가 선택하면 서버에 전송하는 방식.
+            //자신 보다 먼저온 플레이어가 말을 선택하지 않았으면 다른 사람들은 선택을 하지 못하도록 서버에서 통제.
             if (data.Equals("1"))
             {
                 BLUE = true;
@@ -105,24 +110,33 @@ namespace boardgame
                 //server.send("4번");
 
             }
+
+
+
+            // 자신의 턴이 끝났을 때 서버에 턴이 끝났음을 알림.
             if (data.Equals("END TURN"))
             {
                 Turn = false;
                 server.send("NEXT");
             }
+            //서버가 내 턴이라고 메시지를 보내오면 자신의 턴으로 변경.
             if (data.Equals("YOUR TURN"))
             {
                 Turn = true;
             }
+
+            //서버가 게임을 시작하라고 하면 게임화면을 변경.
             if (data.Equals("GameStart"))
             {
                 Form_show();
             }
+
+            //게임 도중 건물을 구매했을 경우 내가 구매한 구역의 좌표를 보내고 다른 플레이어가 구매를 하였으면 그 좌표와 얼마나 지었는지를 서버에서 받아 자신의 메인 폼에 적용.
             if (data.Contains("bui"))
             {
-
+                //추가 및 받는 값 변경필요.
             }
-            if (data.Contains("loc")) // 다른 클라이언트의 위치 받음
+            if (data.Contains("loc")) // 다른 클라이언트의 위치 받음. 현재 이동 메소드를 전체적으로 변경하여 변경필요.
             {
                 string[] dataspl = data.Split(new char[] { ' ' });
                 string[] numspl = dataspl[0].Split(new string[] { "번" }, StringSplitOptions.None);
